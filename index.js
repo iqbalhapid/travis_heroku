05bf94a65524fc4de71daafecb7892fc046de4ec
iqbalhapid_Dkatalis <iqbal.hafid@dkatalis.com>
2020-01-20 16:51:58 +0700

    added server return hello world

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const hapi = require('@hapi/hapi')
+
+const server = new hapi.Server({
+    port: 8080,
+    host: "0.0.0.0"
+})
+
+server.route({
+    method : 'GET',
+    path : '/',
+    handler : (request, h) => {
+    return h.response('hello world')
+    }
+})
+
+const start = async () => {
+    await server.start()
+    console.log('Server running at: ', server.info.uri)
+    return server
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err)
+    process.exit(1)
+})
+
+start()
